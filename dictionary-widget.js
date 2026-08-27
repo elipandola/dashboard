fded60c54e46fc0f72cf41e7f70d3385fb7349ec
Oliver Merida Carreño <meridaoliver6@gamil.com>
2026-08-27 14:16:56 -0400

    Oliver Merida, Nahuel Pairumani Jhaelin Delgadillo: Iteracion 1 - Estructura estatica del widget diccionario ingles

---
+++ dictionary-widget.js
@@ -0,0 +1,25 @@
+/**
+ * Widget 4: Diccionario en inglés (Iteración 1)
+ */
+function mountDictionaryWidget(containerId) {
+  const container = document.getElementById(containerId);
+
+  if (!container) {
+    console.warn(`No se encontró el contenedor #${containerId}`);
+    return;
+  }
+
+  container.innerHTML = `
+    <span class="tag">Diccionario inglés</span>
+    <div class="dictionary-box">
+      <div class="word-header">
+        <h3 class="word-title">Developer</h3>
+        <span class="word-type">noun</span>
+      </div>
+      <ul class="word-definitions">
+        <li>A person that develops software or multimedia applications.</li>
+        <li>A person or entity that creates or enhances something.</li>
+      </ul>
+    </div>
+  `;
+}
