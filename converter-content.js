0e9c9a60780aaf82ca213666cc708d446fad67ba
Brandon Ovando <brandon.ovando@ucb.edu.bo>
2026-08-28 09:49:18 -0400

    Agregue el conversor

---
+++ converter-content.js
@@ -0,0 +1,11 @@
+function getConverterContent() {
+  return `
+    <span class="tag">Conversor</span>
+
+    <div class="conversion-static">
+      <span>1 metro</span>
+      <span>=</span>
+      <span>100 centímetros</span>
+    </div>
+  `;
+}
